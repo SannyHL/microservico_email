472ead819696f9b0b393c6a5f64ba27cb6279ec0
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-08 13:15:42 -0300

    Inserção do @Service e da injeção de dependências.

--- microservice_email/src/main/java/com/ms_email/microservice_email/services/MsEmailService.java
+++ microservice_email/src/main/java/com/ms_email/microservice_email/services/MsEmailService.java
@@ -1,4 +1,13 @@
 package com.ms_email.microservice_email.services;
 
+import com.ms_email.microservice_email.repositories.MsEmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MsEmailService {
+
+    @Autowired
+    private MsEmailRepository msEmailRepository;
+
 }
