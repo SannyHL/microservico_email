0c2955f72e9bb844fe61b4b01d2c21176ed0eb30
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-08 13:15:04 -0300

    Inserção do @Repository.

--- microservice_email/src/main/java/com/ms_email/microservice_email/repositories/MsEmailRepository.java
+++ microservice_email/src/main/java/com/ms_email/microservice_email/repositories/MsEmailRepository.java
@@ -2,8 +2,10 @@ package com.ms_email.microservice_email.repositories;
 
 import com.ms_email.microservice_email.models.MsEmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MsEmailRepository extends JpaRepository<MsEmailModel, UUID> {
 }
