790f2913cd40e15f55fcfdac464c83bf4e815b59
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-08 13:20:20 -0300

    Inserção de atributos.

--- microservice_email/src/main/java/com/ms_email/microservice_email/dtos/MsEmailDto.java
+++ microservice_email/src/main/java/com/ms_email/microservice_email/dtos/MsEmailDto.java
@@ -1,4 +1,20 @@
 package com.ms_email.microservice_email.dtos;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
 public class MsEmailDto {
+
+    @NotBlank
+    private String pessoaEmail;
+    @NotBlank
+    private String emissorEmail;
+    @NotBlank
+    private String destinatarioEmail;
+    @NotBlank
+    private String tituloEmail;
+    @NotBlank
+    private String texto;
 }
