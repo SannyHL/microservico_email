41b19a620c7bac669ec05fa542a56ef57d487039
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-08 13:13:24 -0300

    Extendendo o repositório.

--- microservice_email/src/main/java/com/ms_email/microservice_email/repositories/MsEmailRepository.java
+++ microservice_email/src/main/java/com/ms_email/microservice_email/repositories/MsEmailRepository.java
@@ -1,4 +1,9 @@
 package com.ms_email.microservice_email.repositories;
 
-public interface MsEmailRepository {
+import com.ms_email.microservice_email.models.MsEmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MsEmailRepository extends JpaRepository<MsEmailModel, UUID> {
 }
