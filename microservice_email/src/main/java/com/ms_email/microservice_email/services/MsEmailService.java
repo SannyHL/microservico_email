7f67e3f6ffaa7daae16e7b93ba9cd00c84e6ffe8
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-08 15:06:04 -0300

    Criação método create.

--- microservice_email/src/main/java/com/ms_email/microservice_email/services/MsEmailService.java
+++ microservice_email/src/main/java/com/ms_email/microservice_email/services/MsEmailService.java
@@ -1,13 +1,36 @@
 package com.ms_email.microservice_email.services;
 
+import com.ms_email.microservice_email.enums.StatusEnvioEmail;
+import com.ms_email.microservice_email.models.MsEmailModel;
 import com.ms_email.microservice_email.repositories.MsEmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MsEmailService {
 
     @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
     private MsEmailRepository msEmailRepository;
 
+
+    public MsEmailModel create(MsEmailModel msEmailModel) {
+        try {
+            var mensagem = new SimpleMailMessage();
+            mensagem.setFrom(msEmailModel.getEmissorEmail());
+            mensagem.setTo(msEmailModel.getDestinatarioEmail());
+            mensagem.setSubject(msEmailModel.getTituloEmail());
+            mensagem.setText(msEmailModel.getTexto());
+            javaMailSender.send(mensagem);
+            msEmailModel.setStatusEnvioEmail(StatusEnvioEmail.ENVIADO);
+        } catch (MailException exception){
+            msEmailModel.setStatusEnvioEmail(StatusEnvioEmail.ERRO);
+        } finally {
+            return msEmailRepository.save(msEmailModel);
+        }
+    }
 }
