1546bd5434c5636f0eade48f7aad90aaf4037347
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-08 13:09:10 -0300

    Criação de entidade.

--- microservice_email/src/main/java/com/ms_email/microservice_email/models/MsEmailModel.java
+++ microservice_email/src/main/java/com/ms_email/microservice_email/models/MsEmailModel.java
@@ -1,4 +1,32 @@
 package com.ms_email.microservice_email.models;
 
+import com.ms_email.microservice_email.enums.StatusEnvioEmail;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tbemail")
 public class MsEmailModel {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID emailId;
+
+    private String pessoaEmail;
+    private String emissorEmail;
+    private String destinatarioEmail;
+    private String tituloEmail;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String texto;
+    private StatusEnvioEmail statusEnvioEmail;
+    private LocalDateTime dataEnvioEmail;
+
+
+
+
 }
