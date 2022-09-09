2c28a98267df6e61d9eff9141ad3489de5f43a05
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-08 21:59:23 -0300

    modificando tipo id

--- microservice_email/src/main/java/com/ms_email/microservice_email/repositories/MsEmailRepository.java
+++ microservice_email/src/main/java/com/ms_email/microservice_email/repositories/MsEmailRepository.java
@@ -4,8 +4,7 @@ import com.ms_email.microservice_email.models.MsEmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
 @Repository
-public interface MsEmailRepository extends JpaRepository<MsEmailModel, UUID> {
+public interface MsEmailRepository extends JpaRepository<MsEmailModel, Long> {
 }
